07cf1a9e8b1d95ca5b842250873a6c8938eaa91d
tetuya1231 <takeyasu_1231_1231@outlook.com>
2024-06-25 13:11:02 +0900

    chore: temporarily comment out ppr incremental due to error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export const experimental_ppr = true;
+// TODO: エラーが発生されるため一時的にコメントアウト
+// export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
